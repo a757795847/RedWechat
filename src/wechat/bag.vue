69f218c32f0c8e5eeb8f9933d1f5256c49a7a2b2
zlm <qazse41@sina.cn>
2017-04-21 16:07:46 +0800

    上传图片调试

--- src/wechat/bag.vue
+++ src/wechat/bag.vue
@@ -121,11 +121,7 @@
                     sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                     success: function (res) {
                         var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-
-
-                        console.log('chooseImage=>',localIds);
                         localIds.map(function (id){
-                            console.log('localIds.map=>',id);
                             wx.uploadImage({
                                 localId: id, // 需要上传的图片的本地ID，由chooseImage接口获得
                                 isShowProgressTips: 1, // 默认为1，显示进度提示
@@ -156,7 +152,7 @@
                 if (res.body.status == "1") {
                     this.config = res.body;
                     wx.config({
-                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                         appId: this.config.appid, // 必填，公众号的唯一标识
                         timestamp: this.config.timestamp, // 必填，生成签名的时间戳
                         nonceStr: this.config.nonceStr, // 必填，生成签名的随机串
